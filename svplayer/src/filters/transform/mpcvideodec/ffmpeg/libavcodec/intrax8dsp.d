8dbe43932adf920f33288f4edfe9ba321087a15c
admin <admin@localhost>
2009-03-26 15:56:55 +0800

    [svn r131]

---
+++ svplayer/src/filters/transform/mpcvideodec/ffmpeg/libavcodec/intrax8dsp.d
@@ -0,0 +1,6 @@
+libavcodec/intrax8dsp.o: libavcodec/intrax8dsp.c libavcodec/dsputil.h \
+  libavcodec/avcodec.h ffcodecs.h ffImgfmt.h libavutil/common.h config.h \
+  libavutil/internal.h libavutil/common.h libavutil/intreadwrite.h \
+  libavutil/bswap.h libavutil/x86/bswap.h libavutil/mem.h \
+  libavutil/avutil.h libavutil/mathematics.h libavutil/rational.h \
+  libavutil/log.h

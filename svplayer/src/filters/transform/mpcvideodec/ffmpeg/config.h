bcf3655f49a96d1122386497f194dd9363143b75
admin <admin@localhost>
2009-07-19 03:33:16 +0800

    [svn r466] remove sse3 optimize for ffmpeg

--- svplayer/src/filters/transform/mpcvideodec/ffmpeg/config.h
+++ svplayer/src/filters/transform/mpcvideodec/ffmpeg/config.h
@@ -3,9 +3,9 @@
 	#define HAVE_INLINE_ASM 1
   #define HAVE_MMX 1
   #define HAVE_SSE 1
-  #define HAVE_SSSE3 1
-  #define HAVE_AMD3DNOW 1
-  #define HAVE_AMD3DNOWEXT 1
+  #define HAVE_SSSE3 0
+  #define HAVE_AMD3DNOW 0
+  #define HAVE_AMD3DNOWEXT 0
   #define ARCH_X86 1  
   #ifdef ARCH_X86_64
     #define HAVE_FAST_64BIT 1

3d8786eedf705803644e65fada0bed8e234681d2
admin <admin@localhost>
2009-09-24 06:42:51 +0800

    [svn r714] ....

--- svplayer/src/filters/transform/svpfilter/stdafx.h
+++ svplayer/src/filters/transform/svpfilter/stdafx.h
@@ -1,18 +1,1 @@
-#pragma once
-
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
-#define WINVER			0x0600
-
-#ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
-#endif
-
-#include <afx.h>
-#include <afxwin.h>         // MFC core and standard components
-
-// TODO: reference additional headers your program requires here
-
-#include <streams.h>
-#include <dvdmedia.h>
-#include <amvideo.h>
+#pragma once

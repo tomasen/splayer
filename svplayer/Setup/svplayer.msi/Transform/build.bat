067eb3fd5606a9a68f5a1968e52ba5b71ae638cc
michaelhe <michaelhe@localhost>
2009-12-13 11:11:48 +0800

    [svn r1023] add CHT into the final MSI file.

--- svplayer/Setup/svplayer.msi/Transform/build.bat
+++ svplayer/Setup/svplayer.msi/Transform/build.bat
@@ -1,11 +1,11 @@
 REM http://www.installsite.org/pages/en/msi/articles/embeddedlang/index.htm
 msitran -g ..\wix\bin\Release\en-US\SvPlayer.msi ..\wix\bin\ReleaseCHS\zh-CN\SvPlayer.msi chs.mst
-
-rem msidb -d bin\Release\en-US\SvPlayer.msi -r chs.mst
+msitran -g ..\wix\bin\Release\en-US\SvPlayer.msi ..\wix\bin\ReleaseCHT\zh-TW\SvPlayer.msi cht.mst
 
 del /F ..\..\..\..\SPlayer.msi
 copy /y ..\wix\bin\Release\en-US\SvPlayer.msi ..\..\..\..\SPlayer.msi
 
 scripts\wisubstg.vbs ..\..\..\..\SPlayer.msi chs.mst 2052
+scripts\wisubstg.vbs ..\..\..\..\SPlayer.msi cht.mst 1028
 
-scripts\WiLangId.vbs ..\..\..\..\SPlayer.msi Package 1033,2052
+scripts\WiLangId.vbs ..\..\..\..\SPlayer.msi Package 1033,2052,1028

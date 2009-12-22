a035e3cee42b9c949ddcd5f641cd8e6e6aa72ac2
admin <admin@localhost>
2009-12-22 20:40:38 +0800

    [svn r1048] ??DivX H.264????

--- svplayer/src/apps/mplayerc/FakeFilterMapper2.cpp
+++ svplayer/src/apps/mplayerc/FakeFilterMapper2.cpp
@@ -594,7 +594,10 @@ LONG WINAPI Mine_RegQueryValueExA(HKEY a0, LPCSTR a1, LPDWORD a2, LPDWORD a3, LP
 		return ERROR_SUCCESS;
 		}
 		
-	
+		if(_strcmpi(a1, "AVC 7x DisplayOrder") == 0){
+			*(DWORD*)a4 = 0;
+			return ERROR_SUCCESS;
+		}
 		if(_strcmpi(a1, "AVC 7x Logo") == 0){
 			*(DWORD*)a4 = 0;
 			return ERROR_SUCCESS;

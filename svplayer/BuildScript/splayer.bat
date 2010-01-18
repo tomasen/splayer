ddebd0a558cbbf3c60946ed683c2c0e181466167
michaelhe <michaelhe@localhost>
2010-01-18 22:47:36 +0800

    [svn r1154] Use MSbuild from Framework 3.5 to build VS2008 solution.

--- svplayer/BuildScript/splayer.bat
+++ svplayer/BuildScript/splayer.bat
@@ -1,1 +1,1 @@
-msbuild splayer.proj /l:FileLogger,Microsoft.Build.Engine;logfile=splayer.log;verbosity=detailed
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild" splayer.proj /l:FileLogger,Microsoft.Build.Engine;logfile=splayer.log;verbosity=detailed

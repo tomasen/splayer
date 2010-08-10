92322305524ca1b3b99cc04c993c12ffda120a77
billholt <billholt@localhost>
2010-08-10 14:03:24 +0800

    [svn r1403] + Removal of OptionHotkeyPage due to design change + Removed "Apply" "Context Help" buttons from OptionDlg + Related resource changes

--- splayer/branches/dda0001/src/apps/mplayerc/UserInterface/Dialogs/OptionDlg_Win.cc
+++ splayer/branches/dda0001/src/apps/mplayerc/UserInterface/Dialogs/OptionDlg_Win.cc
@@ -4,7 +4,6 @@
 #include "OptionSubtitlePage_Win.h"
 #include "OptionAdvancedPage_Win.h"
 #include "OptionAssociationPage_Win.h"
-#include "OptionHotkeyPage_Win.h"
 
 OptionDlg::OptionDlg(void)
 {
@@ -12,13 +11,13 @@ OptionDlg::OptionDlg(void)
   m_subtitlepage    = new OptionSubtitlePage();
   m_advancedpage    = new OptionAdvancedPage();
   m_associationpage = new OptionAssociationPage();
-  m_hotkeypage      = new OptionHotkeyPage();
+
+  m_psh.dwFlags |= PSH_NOAPPLYNOW | PSH_NOCONTEXTHELP;
 
   AddPage(*m_basicpage);
   AddPage(*m_subtitlepage);
   AddPage(*m_advancedpage);
   AddPage(*m_associationpage);
-  AddPage(*m_hotkeypage);
 }
 
 OptionDlg::~OptionDlg(void)
@@ -27,5 +26,4 @@ OptionDlg::~OptionDlg(void)
   delete m_subtitlepage;
   delete m_advancedpage;
   delete m_associationpage;
-  delete m_hotkeypage;
 }

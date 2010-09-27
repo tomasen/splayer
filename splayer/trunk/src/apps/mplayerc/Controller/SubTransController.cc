99dc3c5a95567cea2ae76cdb2ac61b50d3e917ae
billholt <billholt@localhost>
2010-09-27 15:15:04 +0800

    [svn r1547] * Added sinet dependency (requires new dev package to work)

--- splayer/trunk/src/apps/mplayerc/Controller/SubTransController.cc
+++ splayer/trunk/src/apps/mplayerc/Controller/SubTransController.cc
@@ -1,2 +1,72 @@
 #include "stdafx.h"
 #include "SubTransController.h"
+#include <sinet.h>
+
+using namespace sinet;
+
+SubTransController::SubTransController(void):
+  m_stopevent(::CreateEvent(NULL, TRUE, FALSE, NULL)),
+  m_thread(NULL),
+  m_operation(Unknown)
+{
+  refptr<pool> pool = pool::create_instance();
+}
+
+SubTransController::~SubTransController(void)
+{
+  Stop(true);
+}
+
+void SubTransController::SetFrame(HWND hwnd)
+{
+
+}
+
+void SubTransController::Start(const wchar_t* video_filename, 
+                               SubTransOperation operation, 
+                               StringList files_upload /*= StringList()*/)
+{
+  // we should stop running tasks first
+  Stop(true);
+  // record parameters
+  m_operation = operation;
+  // create thread
+  m_thread = (HANDLE)::_beginthread(_thread_dispatch, 0, (void*)this);
+}
+
+void SubTransController::Stop(bool synced /*= false*/)
+{
+  ::SetEvent(m_stopevent);
+  if (synced && m_thread && m_thread != INVALID_HANDLE_VALUE)
+  {
+    ::WaitForSingleObject(m_thread, INFINITE);
+    m_thread = NULL;
+  }
+}
+
+void SubTransController::_thread_dispatch(void* param)
+{
+  static_cast<SubTransController*>(param)->_thread();
+}
+
+void SubTransController::_thread()
+{
+  switch (m_operation)
+  {
+  case DownloadSubtitle:
+    _thread_download();
+    break;
+  case UploadSubtitle:
+    _thread_upload();
+    break;
+  }
+}
+
+void SubTransController::_thread_download()
+{
+}
+
+void SubTransController::_thread_upload()
+{
+  refptr<pool> pool = pool::create_instance();
+}
--- splayer/trunk/src/apps/mplayerc/Controller/SubTransController.h
+++ splayer/trunk/src/apps/mplayerc/Controller/SubTransController.h
@@ -4,6 +4,36 @@
 class SubTransController
 {
 public:
+  SubTransController(void);
+  ~SubTransController(void);
+
+  typedef enum {
+    Unknown,
+    DownloadSubtitle,
+    UploadSubtitle
+  } SubTransOperation;
+
+  typedef std::vector<std::wstring> StringList;
+
+  void SetFrame(HWND hwnd);
+
+  // starting and ending main thread for upload / download
+  void Start(const wchar_t* video_filename, SubTransOperation operation,
+             StringList files_upload = StringList());
+  void Stop(bool synced = false);
+
+  // primary thread logics, should not be called directly
+  static void _thread_dispatch(void* param);
+  void _thread();
+
+private:
+  void _thread_download();
+  void _thread_upload();
+
+  SubTransOperation m_operation;
+
+  HANDLE  m_thread;
+  HANDLE  m_stopevent;
 };
 
 #endif // SUBTRANSCONTROLLER_H

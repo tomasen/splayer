1dcd331dce38f044f527fce07f0f93f44d334429
Tomasen <Tomasen@localhost>
2010-12-10 06:58:39 +0800

    fix: remove more debug level logging

--- src/svplib/svplib.h
+++ src/svplib/svplib.h
@@ -19,11 +19,28 @@
 
 #include <logging.h>
 
+#ifndef SVP_LogMsg
 #define SVP_LogMsg(x, ...)  Logging(x)
+#endif
+
+#ifndef SVP_LogMsg2
 #define SVP_LogMsg2(...) Logging(__VA_ARGS__)
+#endif
+
+#ifndef SVP_LogMsg3
 #define SVP_LogMsg3(...) Logging(__VA_ARGS__)
+#endif
+
+#ifndef SVP_LogMsg4
 #define SVP_LogMsg4(...) Logging(__VA_ARGS__)
+#endif
+
+#ifndef SVP_LogMsg5
 #define SVP_LogMsg5(...) Logging(__VA_ARGS__)
+#endif
+
+#ifndef SVP_LogMsg6
 #define SVP_LogMsg6(...) Logging(__VA_ARGS__)
+#endif
 
 bool SVP_CanUseCoreAvcCUDA(bool useCUDA);

29112c2e9e9ace4767ff48f508fc0fcc24a870f1
Tomasen <Tomasen@localhost>
2010-12-27 14:27:56 +0800

    Ignore system rmvb decoder <<Issue 15>>

--- src/apps/mplayerc/FGManager.cpp
+++ src/apps/mplayerc/FGManager.cpp
@@ -745,9 +745,9 @@ STDMETHODIMP CFGManager::Connect(IPin* pPinOut, IPin* pPinIn)
 			if (szFName.Find(_T("UUSEE DeMultiplexer")) >= 0 ) continue;
 			if (szFName.Find(_T("VideoTune")) >= 0 ) continue;
 			if (szFName.Find(_T("Sonic HD Demuxer")) >= 0 ) continue;
-            if (szFName.Find(_T("PPS - RealVideo Decoder")) >= 0 ) continue;
-            if (szFName.Find(_T("Thunder RM Video Decoder")) >= 0 ) continue;
-            if (szFName.Find(_T("Roxio Mp3 Encoder")) >= 0 ) continue;
+      if (szFName.Find(_T("PPS - RealVideo Decoder")) >= 0 ) continue;
+      if (szFName.Find(_T("Thunder RM Video Decoder")) >= 0 ) continue;
+      if (szFName.Find(_T("Roxio Mp3 Encoder")) >= 0 ) continue;
 			
 			//if (szFName.Find(_T("SHN to Wave Filter")) >= 0 ) continue;
 			//if (szFName.Find(_T("AVI Decompressor (YV12)")) >= 0 ) continue;
@@ -765,6 +765,9 @@ STDMETHODIMP CFGManager::Connect(IPin* pPinOut, IPin* pPinIn)
 
 				if (szFName.Find(_T("DirectVobSub")) >= 0 ) continue;
 				
+        if (FGID == GUIDFromCString(_T("{238D0F23-5DC9-45A6-9BE2-666160C324DD}"))  ) continue; //"RealVideo Decoder"
+        if (FGID == GUIDFromCString(_T("{238D0F23-5DC9-45A7-9BE2-666160C324DD}"))  ) continue; //"RealVideo Decoder"
+        
 				if (FGID == GUIDFromCString(_T("{48CF8992-4161-49D6-9A9B-F1FDB3BAE74D}"))  ) continue; //"UUSEE DeMultiplexer"
               
 				//if (FGID == GUIDFromCString(_T("{E8D381DD-8C7D-4a6f-96ED-92BBB64064CF}"))  ) continue; SVPSubFilter

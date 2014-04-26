80c40c470c74d91897a90df167a73bbc03e592bf
Jonas Yang <Jonas Yang@localhost>
2014-04-26 23:13:09 +0800

    Fix post command for stampping version.

--- BuildScript/post-build.cmd
+++ BuildScript/post-build.cmd
@@ -4,4 +4,4 @@ chdir /D %~dp0
 
 for /f "delims=+ " %%a in ('hg_bin\hg id -n -R ../') do @set revnum=%%a 
 
-StampVer.exe -f"3.7.0.%revnum%" -p"3.7.0.%revnum%" ../../splayer.exe
+StampVer.exe -f"3.7.0.%revnum%" -p"3.7.0.%revnum%" ../out/bin/splayer.exe

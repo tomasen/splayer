fb3853a81bb5235d6024456e1d239d2b35cc675c
Tomasen <tomasen@gmail.com>
2017-06-19 20:31:47 +0800

    make nsis works for netSetup.nsi

---
+++ Setup/csfcodec/install.bat
@@ -0,0 +1,16 @@
+regsvr32 /s mpc_mdssockc.dll
+regsvr32 /s mpc_mxaudio.dll
+regsvr32 /s mpc_mxvideo.dll
+regsvr32 /s mpc_mxscreen.dll
+regsvr32 /s mpc_mxshbasu.dll
+regsvr32 /s mpc_mxshmaiu.dll
+regsvr32 /s mpc_mxshsour.dll
+regsvr32 /s mpc_wtlvcl.dll
+regsvr32 /s mpc_mcucltu.dll
+regsvr32 /s mpc_mcufilecu.dll
+regsvr32 /s mpc_mtcontrol.dll
+regsvr32 /s mpc_mtcontain.dll
+regsvr32 /s mpc_mxsource.dll
+regsvr32 /s mpc_mxrender.dll
+
+ 
